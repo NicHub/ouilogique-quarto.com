872edfa21998594a6b2bfd844eca6a68fcaddee4
NicHub <nicolas.jeanmonod@gmail.com>
2023-10-05 15:34:25 +0200

    update

--- _scripts/copy_pages.py
+++ _scripts/copy_pages.py
@@ -6,27 +6,33 @@ import logging
 import os
 import shutil
 
-SOURCE_PATH = "./pages"
-DEST_PATH = "./_site/"
+pwd = os.path.abspath(os.curdir)
+prefix = "../" if "_scripts" in pwd else "./"
+SOURCE_PATH = f"{prefix}pages/"
+DEST_PATH = f"{prefix}_site/"
 
 
 def main():
     """___"""
     if not os.path.exists(SOURCE_PATH):
         raise SystemExit(f"{__file__} => {SOURCE_PATH} not found")
-    if not os.path.exists(DEST_PATH):
-        raise SystemExit(f"{__file__} => {SOURCE_PATH} not found")
 
-    for dir in SOURCE_PATH:
+    for dir in os.listdir(SOURCE_PATH):
+        if dir in [".DS_Store"]:
+            continue
+        source_path = f"{SOURCE_PATH}{dir}"
         dest_path = f"{DEST_PATH}{os.path.split(dir)[1]}"
-        shutil.copytree(dir, dest_path, dirs_exist_ok=True)
+        print(source_path)
+        print(dest_path)
+        print()
+        shutil.copytree(source_path, dest_path, dirs_exist_ok=True)
 
 
 try:
     logging.basicConfig(
         format="%(levelno)s " "%(module)-11s " "%(lineno)3s. " "%(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
-        level=logging.INFO
+        level=logging.INFO,
     )
     main()
 except SystemExit as _e:
